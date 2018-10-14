e5d055b1502f7169fe45897078eee435011df46d
Ryan Hikel <hikelryan@gmail.com>
2018-10-14 13:22:46 -0400

    Added email login functionality

---
+++ Main.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
+export default class Main extends React.Component {
+  state = { currentUser: null }
+  componentDidMount() {
+    const { currentUser } = firebase.auth()
+    this.setState({ currentUser })
+  }
+  handleLogOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => this.props.navigation.navigate('Main'))
+      .catch(error => this.setState({ errorMessage: error.message }))
+  }
+  render() {
+    const { currentUser } = this.state
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+        <Button title="Log Out" onPress={this.handleLogOut} />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
